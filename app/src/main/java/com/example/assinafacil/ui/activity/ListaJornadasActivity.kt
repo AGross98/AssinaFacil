5b8579a27a50b5a38c6069c512967b2b360c7f9a
Anderson <anderson.gross98@gmail.com>
2023-09-19 21:17:34 -0300

    Layout historico

---
+++ app/src/main/java/com/example/assinafacil/ui/activity/ListaJornadasActivity.kt
@@ -0,0 +1,12 @@
+package com.example.assinafacil.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.assinafacil.R
+
+class ListaJornadasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_jornadas)
+    }
+}
